01c540e315f4ff57814468d82e68c2d933e2bb6e
Bonaventure <toybpro@gmail.com>
2025-08-19 10:21:26 +0100

    first

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import { UiProvider } from './contexts/UiContext'
 import { CartProvider } from './contexts/CartContext'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ToastContainer } from './components/common/Toast'
-import Loader from './components/common/Loader'
-import AppRoutes from './router/AppRouter'
+//import Loader from './components/common/Loader'
+
 import PublicRoutes from './router/PublicRouter'
-import AuthRoutes from './router/AuthRouter'
+//import AuthRoutes from './router/AuthRouter'
 import ParentRoutes from './router/ParentRouter'
 import ChildRoutes from './router/ChildRouter'
 import AdminRoutes from './router/AdminRouter'
@@ -21,7 +21,7 @@ function App() {
           <CartProvider>
             <Routes>
               <Route path="/*" element={<PublicRoutes />} />
-              <Route path="/auth/*" element={<AuthRoutes />} />
+              {/* <Route path="/auth/*" element={<AuthRoutes />} /> */}
               <Route path="/parent/*" element={<ParentRoutes />} />
               <Route path="/child/*" element={<ChildRoutes />} />
               <Route path="/admin/*" element={<AdminRoutes />} />
